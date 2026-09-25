1bc39d8c83060c8cbe141ad654dd31fc56252ca9
JiaXuan-coder <jiaxuanquah@gmail.com>
2026-09-25 16:04:55 +0800

    Dashboard section 1 (4 charts) completed.

--- Physical Activity in Australia/js/vega_lite_vis.js
+++ Physical Activity in Australia/js/vega_lite_vis.js
@@ -1,8 +1,16 @@
 var top15Spec = "js/top15_activities_participated.json";
 var ageSkewedSpec = "js/age_skewed.json";
+var whoorganised = "js/who_organises_activities.json";
+var technologySpec = "js/technology_gadgets.json";
 
-vegaEmbed('#top15_activities_chart', top15Spec)
+vegaEmbed('#top15_activities_chart', top15Spec, { actions: false })
     .catch(console.error);
 
-vegaEmbed('#age_skewed_chart', ageSkewedSpec)
+vegaEmbed('#age_skewed_chart', ageSkewedSpec, { actions: false })
+    .catch(console.error);
+
+vegaEmbed('#who_organised_chart', whoorganised, { actions: false })
+    .catch(console.error);
+
+vegaEmbed('#technology_gadgets_chart', technologySpec, { actions: false })
     .catch(console.error);
